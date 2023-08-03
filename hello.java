2ca8c9f0b50cd9f4d34df6ff1e7b50e8ecffe2e0
Suchendra <62923881+Suchendra-jpg@users.noreply.github.com>
2023-08-03 13:09:37 +0530

    updated

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class hello{
         public static void main(String[ ] args)
         {
 
-         System.out.println("Assignment 1 build");
+         System.out.println(" Assignment 1 build");
 
         }
